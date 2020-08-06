a4c68ecaa48cfcab5c90804bcd0317f6d144980f
Falko Modler <famod@users.noreply.github.com>
2020-08-07 00:45:43 +0200

    Showcase for validator CNFE for profile dependency

--- support/src/main/java/com/github/famod/modmono_quarkus/support/SupportHelloResource.java
+++ support/src/main/java/com/github/famod/modmono_quarkus/support/SupportHelloResource.java
@@ -1,22 +1,23 @@
-package com.github.famod.modmono_quarkus.dist;
+package com.github.famod.modmono_quarkus.support;
 
 import com.github.famod.modmono_quarkus.core.HelloBean;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class HelloResource {
+@Path("/support-hello")
+public class SupportHelloResource {
 
     @Inject
     HelloBean bean;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public @NotNull String hello() {
         return bean.hello() + "!";
     }
 }
